d2dbfbc293997f8b17d9184df307df3ac83229bc
Blueify <136642460+Bluewify@users.noreply.github.com>
2024-01-26 11:35:01 -0700

    Create main.js
    Main file for the source code.

---
+++ main.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Twilight Client
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Multi-purpose Bloxd.io Client
+// @author       Blueify
+// @match        https://*.bloxd.io
+// @match        https://*.bloxdhop.io/*
+// @match        https://*.bloxdk12.com/*
+// @match        https://*.doodlecube.io/*
+// @match        https://*.eviltower.io/*
+// @icon         https://cdn.discordapp.com/attachments/1191077575326441522/1194025083908407409/Screenshot_2024-01-08_133955.png?ex=65aed983&is=659c6483&hm=bc9f4f9a1ecee0929f72da4e782ae5897bbd0e9f4d98d6c0a852b869ec597cfa&
+// @grant        GM_download
+// @grant        GM_addStyle
+// @license      CC
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+})();
